066c9ad531cea5912d1b731aa8c8866e6cfffcc8
Justin Torrance <justin.torrance91@gmail.com>
2019-04-01 08:30:00 -0600

    Add restart button

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const handleSubmit = (e) => {
   const combineName = firstName + lastName
   const name = combineName.split('')
   netWorth = generateNumber(name)
+  displayGuessSection()
+  document.querySelector('.name-form').style.display='none'
   return netWorth
 }
 
@@ -33,16 +35,37 @@ generateNumber = (name) => {
 
 handleGuess = (e) => {
   e.preventDefault()
-  var feedback = document.querySelector('.feedback')
-  console.log('feedback', feedback)
-  const guessInput = parseInt(document.querySelector('.guess-input').value) 
-  if (guessInput > netWorth) {
-    feedback.innerText = `Keep dreaming, That's too high! Guess again`
-  } else if (guessInput < netWorth) {
-    feedback.innerText = `Good news, that\'s that's too low! Guess again`
-  } else {
-    feedback.innerText = `That's correct! What a fortune! Now go earn your millions`
+  if (e.target.classList.contains('guess-btn')) {
+    var feedback = document.querySelector('.feedback')
+    const guessInput = parseInt(document.querySelector('.guess-input').value) 
+    if (guessInput > netWorth) {
+      feedback.innerText = `Keep dreaming, That's too high! Guess again`
+    } else if (guessInput < netWorth) {
+      feedback.innerText = `Good news, that\'s that's too low! Guess again`
+    } else {
+      feedback.innerText = `That's correct! What a fortune! Now go earn your millions`
+    }
+  } else if (e.target.classList.contains('restart-btn')) {
+      location.reload();
   }
 }
 
-document.querySelector('.guess-btn').addEventListener('click', handleGuess)
+document.querySelector('.guess-section').addEventListener('click', handleGuess)
+
+displayGuessSection = () => {
+    document.querySelector('.guess-section').innerHTML = 
+    ` <p class="enter-p">Guess your predicted net worth below!</p>
+    <form class="guess-form">
+      <p class="worth-p">I'll be worth</p>
+      <input  type="number"
+              class='guess-input'
+              name='guess-input'
+      >
+      <p class="million-p">million dollars</p>
+      <button class="guess-btn btn">Enter</button>
+    </form>
+    <p class="feedback"></p> 
+    <form class="restart-form">
+      <button class="restart-btn btn">Restart</button> 
+    </form> `
+}
